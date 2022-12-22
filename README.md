3e460389f59089bae4b6e25079a2c1f6b719df44
Outs1d3r-NET <feniximperialpro@gmail.com>
2022-12-22 01:54:33 -0300

    One ring to control them all

---
+++ README.md
@@ -0,0 +1,74 @@
+# LogSteam  
+> For get logs in GOCACHE API for free.   
+#   
+[![vars](img/banner.jpg)](https://dribbble.com/AgustianES "Imagem by: AgustianES")
+#   
+This script was created due to the lack of resources available to the information security team where I work in order to achieve some PCI DSS requirements it is necessary to constantly monitor their applications, so what this script does is collect the WAF and FIREWALL logs from Gocache.  
+Notice that Gocache has a perfect solution for this called [Log Stream](https://docs.gocache.com.br/api/api_log_stream/ "logstream doc").  
+I strongly recommend that you purchase this product to collect the logs more efficiently, but if you are having financial problems like me, you can use ```LogSteam``` for this purpose.   
+## Preparing the Environment  
+For continuous execution of the **logsteam** I use a **cron job** scheduled with **crontab**, it runs the **logsteam** every ```X``` minutes which collects the logs in the Gocache every ```Y,Z``` time range.  
+### Configure cron:   
+> https://crontab.guru/#*/5_*_*_*_*   
+```bash
+$ crontab -e
+```
+```
+*/5 * * * * /opt/logsteam/logsteam.py
+```
+```bash
+$ service cron restart
+$ service cron status
+$ systemctl enable cron
+```  
+### Configure output data:   
+> https://docs.aws.amazon.com/cli/index.html   
+> https://docs.aws.amazon.com/AmazonS3/latest/userguide/creating-bucket.html   
+The SIEM solution I currently use is collecting data from a BucketS3, so an AWS key pair is needed to send the logs to a BucketS3.  
+* AWS CLI - Configure profile:  
+```bash
+$ sudo apt install awscli -y
+$ aws configure --profile logsteam
+```  
+## Variables:  
+Now let's talk about the logsteam variables:  
+#   
+[![vars](img/00.png)]()
+#   
+From line ```11``` to ```19``` the logsteam variables are configured.   
+
+On line ```12``` we have the gocache API endpoint, more about that [here](https://docs.gocache.com.br/api/).   
+
+On line ```13``` the gocache API token is set, you must generate this with a user administrator on the platform, you can do this directly in the gocache console GUI or via [terminal](https://docs.gocache.com.br/api/api_token_de_api/).   
+
+Line ```14``` is the event limit we will request from the API.  
+
+Line ```15``` is the output directory that logsteam will store the ```.json``` containing the **WAF** and **FIREWALL** data from the API.  
+
+Line ```16``` is a date tag to create a folder with the current day's date to store and organize the logs.  
+
+Line ```17``` is the status folder, where I store the event IDs to prevent repeated events from going to SIEM.  
+
+Line ```18``` is the name of the bucket3 that we will upload the data to SIEM to collect.  
+
+On line ```19``` we have the name of the awscli profile previously configured.   
+
+### Timestamp setting:    
+On line ```50``` of **logsteam**, in the function ```gettimestamp()``` what is happening is that **logsteam** collects the current time and converts it to ```timestamp format``` and adds this value to the variable ```enddate```, then it performs a subtraction calculation on the timestamp of the ```enddate``` and stores the result in the variable ```startdate```, this to adjust the collection of events in the Gocache. Be sure to perform a test on the server to make sure that the timestamp is aligned with the Gocache events:   
+#   
+[![vars](img/01.png)]()
+#   
+```bash
+$ python3
+>>> from datetime import datetime  
+>>> enddate = datetime.now().date().strftime('%m/%d/%y '+str(datetime.now().time()).split('.')[0])
+>>> enddate = str(datetime.strptime(enddate, '%m/%d/%y %H:%M:%S').timestamp()).split('.')[0]
+>>> startdate = str(int(enddate)-600)
+>>> startdate
+'1671683597' 
+>>> enddate
+'1671684197'
+```
+Check in: https://timestamp.online   
+#   
+:frog:
---
+++ logsteam.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+# coding: latin-1
+
+##--> LIBRARIES
+from datetime import datetime
+import requests
+import json
+import time
+import os
+
+##--> VARIABLES
+url = 'https://api.gocache.com.br/v1/' ###--> URL API END POINT
+api_token = 'API_TOKEN_HERE' ###--> API TOKEN GOCACHE: <user_name>
+limdata = '1000' ###--> QTT OF EVENTS
+path_output = '/opt/API/GOCACHE/' ###--> PATH FOR OUTPUT DATA 
+path_domain_date = str(datetime.now().date()).replace('-','_') ###--> FOR CREATE DIR WITH CURRENT DATE NAME
+path_status = path_output+'status/' ###--> PATH FOR STATUS OUTPUT DATA
+buckets3 = 'BUCKET_NAME' ###--> OUTPUT BUCKETS3
+awsprofile = 'PROFILE_NAME' ###--> AWS PROFILE
+
+##--> FUNCTIONS
+###--> GET ALL DOMAINS IN GOCACHE API
+def getdomains():
+
+    global domains
+
+    headers = {'Accept': 'application/json','GoCache-Token': api_token}
+    response = requests.request(method='GET', url=url+"domain", headers=headers)
+    decodedResponse = json.loads(response.text)
+
+    domains = decodedResponse['response']['domains']
+
+    #### CREATE PATHS FOR OUTPUT DATA FOR ALL DOMAINS
+    for domain in range(len(domains)):
+        try:
+            os.makedirs(os.path.dirname(path_output+"domains/"+str(domains[domain])+"/"+path_domain_date+"/"))
+        except:
+            continue
+
+###--> CREATE TIMESTAMPS FOR REQUESTS
+def gettimestamp():
+
+    global startdate
+    global enddate
+    global id_output
+
+    enddate = datetime.now().date().strftime('%m/%d/%y '+str(datetime.now().time()).split('.')[0])
+    enddate = str(datetime.strptime(enddate, '%m/%d/%y %H:%M:%S').timestamp()).split('.')[0]
+
+    startdate = str(int(enddate)-600) ####--> REMOVE 15 MIN FOR START DATE
+    #print("Data de inicio:",startdate) ####--> FOR DEBUG
+    #print("Date de fim:",enddate) ####--> FOR DEBUG
+    try:    ####--> CREATE OUTPUT IDs DB
+        os.makedirs(os.path.dirname(path_status))
+        id_output = open(path_status+'IDs_output.txt','a')
+        id_output.close()
+    except:
+        pass
+
+###--> GET EVENTS IN GOCACHE
+def getfirewalldata():
+    for domain in range(len(domains)): ####--> SCROLL ALL DOMAINS
+        param = {'offset':'0', 'limit':limdata, 'start_date':startdate, 'end_date':enddate}
+        headers = {'Accept': 'application/json','GoCache-Token': api_token}
+        response = requests.request(method='GET', url=url+"firewall/event/"+str(domains[domain]), params=param, headers=headers)
+        decodedResponse = json.loads(response.text)
+
+        time.sleep(1) ####--> BYPASS RATELIMIT GOCACHE
+        
+        ####--> GET EVENT'S NUMBER'S
+        if len(decodedResponse['response']['events']) <= 1:
+            try:    ####--> FOR ONE
+                eventID = str(decodedResponse['response']['events'][0]['id'])
+                eventTSTAMP = str(decodedResponse['response']['events'][0]['timestamp'])
+            except:    ####--> FOR NOTHING
+                #print("[*] Exception 1 - No events found for this domain:",str(domains[domain])) ####--> FOR DEBUG
+                pass 
+        else:
+            for evt in range(len(decodedResponse['response']['events'])):
+                try:    ####--> FOR MULTIPLE EVENTS
+                    eventID = str(decodedResponse['response']['events'][evt]['id'])
+                    eventTSTAMP = str(decodedResponse['response']['events'][evt]['timestamp'])
+
+                    ####--> VERIFY IF EVENT ID IS NEW
+                    with open(path_status+'IDs_output.txt') as ver:
+                        if not eventID in ver.read():
+                            param = {'timestamp':eventTSTAMP}
+                            headers = {'Accept': 'application/json','GoCache-Token': api_token}
+                            response = requests.request(method='GET', url=url+"firewall/event/"+str(domains[domain])+"/"+eventID, params=param, headers=headers)
+                            decodedResponseEvent = json.loads(response.text)
+
+                            ####--> OUTPUT JSON EVENT DATA
+                            eventOUTPUT = open(path_output+"domains/"+str(domains[domain])+"/"+path_domain_date+"/"+str(domains[domain])+"_"+''.join(str(datetime.now().time()).split('.')[0].split(':')[0:2])+'.json','a')
+                            eventOUTPUT.write(str(json.dumps(decodedResponseEvent))+"\n")
+                            eventOUTPUT.close()
+
+                            ####--> REGISTER EVENT ID IN FILE DB
+                            idREG = open(path_status+'IDs_output.txt','a')
+                            idREG.write(eventID+"\n")
+                            idREG.close()
+
+                            time.sleep(1) ####--> BYPASS RATELIMIT GOCACHE
+                except:
+                    #print("[*] Exception 2 - I still don't understand your reason!") ####--> FOR DEBUG
+                    pass 
+
+def uploadtobuckets3():
+    os.system("aws s3 sync "+path_output+"domains "+"s3://"+buckets3+"/ --profile "+awsprofile)
+    time.sleep(5)
+    try:
+        os.system("rm -rf "+path_output+"domains")
+    except:
+        pass
+
+##--> MAIN
+getdomains()
+gettimestamp()
+getfirewalldata()
+time.sleep(5)
+uploadtobuckets3()
